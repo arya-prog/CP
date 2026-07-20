505a78ae99a07dabafd51608f55e967926f3e7ab
arya-prog <146481156+arya-prog@users.noreply.github.com>
2026-07-20 10:56:10 +0530

    2241A - Divide and Conquer

---
+++ 2241ADnC.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+void solve() {
+    int x , y;
+    cin >> x >> y;
+    if(x % y == 0 ){
+        cout << "YES" << endl;
+        return;
+    }
+    else{
+        cout << "NO" << endl;
+        return;
+    }
+      return;
+}
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while(t--) {
+      solve();
+    }
+    return 0;
+}
